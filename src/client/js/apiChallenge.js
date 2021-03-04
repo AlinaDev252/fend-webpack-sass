3fbd1218b0627159313965dc0a1015f08e7d0319
AlinaDev252 <costachealina25@gmail.com>
2021-03-04 10:10:04 +0200

    Add Api Challenge

---
+++ src/client/js/apiChallenge.js
@@ -0,0 +1,19 @@
+function handleSubmit(event) {
+	event.preventDefault();
+
+	// check what text was put into the form field
+	let formText = document.getElementById("name").value;
+
+	Client.checkForName(formText);
+
+	console.log("::: Form Submitted :::");
+	fetch("api.openweathermap.org/data/2.5/weather?q={New York}&appid={6c31832a1258f53fbea517b9e487dcf8}")
+		.then((res) => {
+			return res.json();
+		})
+		.then(function (data) {
+			document.getElementById("results").innerHTML = data.message;
+		});
+}
+
+export { handleSubmit };
